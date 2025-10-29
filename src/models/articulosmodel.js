0b5ca5e233a65526529b40d4aceaaba3a225ecc2
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-29 17:45:05 -0500

    Se realizo la estructura del modelo de articulos, donde se definio los datos para la estructura en la base de datos de MongoDB

--- src/models/articulosmodel.js
+++ src/models/articulosmodel.js
@@ -1,6 +1,37 @@
 const mongoose = require("mongoose");
 const articuloShema = mongoose.Schema({
 
-
+    titulo: {
+        type: String,
+        required: true
+    },
+    autores: {
+        type: String,
+        required: true
+    },
+    anioPublicacion: {
+        type: Date,
+        required: true
+    },
+    resumen: {
+        type: String,
+        required: true
+    },
+    cantReferencia: {
+        type: Number,
+        required: true
+    },
+    nombreBD: {
+        type: String,
+        required: true
+    },
+    nombreRevista: {
+        type: String,
+        required: true
+    },
+    enlace: {
+        type: String,
+        required: true
+    }
 });
 module.exports = mongoose.model("Articulo",articuloShema);
