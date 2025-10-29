812157ca56ced42e5ad272120dc564e77247e1b9
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-28 23:06:22 -0500

    Se empezo a estructurar el archivo articulosmodel, de la carpeta models

--- src/models/articulosmodel.js
+++ src/models/articulosmodel.js
@@ -1,2 +1,6 @@
 const mongoose = require("mongoose");
-const articuloShema = mongoose.Schema();
+const articuloShema = mongoose.Schema({
+
+
+});
+module.exports = mongoose.model("Articulo",articuloShema);
